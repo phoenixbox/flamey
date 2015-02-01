ac41f901d9d3c7b7f8a3eb45876aeaa5361feebf
Shane Rogers <rogerssh@tcd.ie>
2015-01-31 19:03:31 -0800

    Filter slider view

---
+++ Flamey/FLFilterSlider.h
@@ -0,0 +1,13 @@
+//
+//  FLFilterSlider.h
+//  Flamey
+//
+//  Created by Shane Rogers on 1/31/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLFilterSlider : UISlider
+
+@end
---
+++ Flamey/FLFilterSlider.m
@@ -0,0 +1,21 @@
+//
+//  FLFilterSlider.m
+//  Flamey
+//
+//  Created by Shane Rogers on 1/31/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import "FLFilterSlider.h"
+
+@implementation FLFilterSlider
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
