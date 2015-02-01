34de80dd61643a9651c2ae41e377daeb9edf0b47
Shane Rogers <rogerssh@tcd.ie>
2015-01-31 16:10:18 -0800

    Abstracted helper functions to their own class

---
+++ Flamey/FLViewHelpers.h
@@ -0,0 +1,13 @@
+//
+//  FLViewHelpers.h
+//  Flamey
+//
+//  Created by Shane Rogers on 1/31/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FLViewHelpers : NSObject
+
+@end
---
+++ Flamey/FLViewHelpers.m
@@ -0,0 +1,13 @@
+//
+//  FLViewHelpers.m
+//  Flamey
+//
+//  Created by Shane Rogers on 1/31/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import "FLViewHelpers.h"
+
+@implementation FLViewHelpers
+
+@end
