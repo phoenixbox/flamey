a17b12f95eb11d2d235c02edeef6a4e3e4eedfa5
Shane Rogers <rogerssh@tcd.ie>
2015-01-31 19:47:26 -0800

    Create filter helpers

---
+++ Flamey/FilterHelpers.h
@@ -0,0 +1,23 @@
+//
+//  FilterHelpers.h
+//  Flamey
+//
+//  Created by Shane Rogers on 1/31/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#ifndef Flamey_FilterHelpers_h
+#define Flamey_FilterHelpers_h
+
+#define kAdjustTool @"Adjust"
+#define kBrightnessTool @"Brightness"
+#define kContrastTool @"Contrast"
+#define kHighlightsTool @"Highlights"
+#define kShadowsTool @"Shadows"
+#define kSaturationTool @"Saturation"
+#define kVignetteTool @"Vignette"
+#define kWarmthTool @"Warmth"
+#define kTiltShiftTool @"Tilt Shift"
+#define kSharpenTool @"Sharpen"
+
+#endif
