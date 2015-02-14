accf0205b0067481b7cdc733b6f77e9e3c7d4a8c
Shane Rogers <rogerssh@tcd.ie>
2015-02-14 15:11:25 -0800

    Image annotation logic - needs to be cleaned up

--- Flamey/FLImageAnnotationViewController.m
+++ Flamey/FLImageAnnotationViewController.m
@@ -8,13 +8,26 @@
 
 #import "FLImageAnnotationViewController.h"
 
+// Components
+#import "FLFacebookUploadModalViewController.h"
+
 // Pods
 #import <SDWebImage/UIImageView+WebCache.h>
 
+// Data Layer
+#import "FLPhotoStore.h"
+#import "FLProcessedImagesStore.h"
+
+// Pods
+#import "GPUImageLookupFilter.h"
+#import "GPUImagePicture.h"
+#import "GPUImageBrightnessFilter.h"
+#import "GPUImageAlphaBlendFilter.h"
 
 @interface FLImageAnnotationViewController ()
 
 @property (nonatomic, strong) UIImage *facebookImage;
+@property (nonatomic, strong) UITapGestureRecognizer *imageViewTap;
 
 @end
 
@@ -28,22 +41,92 @@
         NSLog(@"imageView phto set");
 
         _facebookImage = image;
+
+        [self addTapGestureRecogniserToImageView];
     }];
 }
+
+- (void)addTapGestureRecogniserToImageView {
+    NSLog(@"SETTING");
+    self.imageViewTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                action:@selector(setFlameIcon:)];
+    self.imageViewTap.numberOfTouchesRequired = 1;
+    self.imageViewTap.numberOfTapsRequired = 1;
+
+    [_photoImageView addGestureRecognizer:self.imageViewTap];
+    // NOTE: Must set interaction true so that the gesture can be triggered. Dont have to have selector on the filter ImageView
+    _photoImageView.userInteractionEnabled = YES;
+}
+
+- (void)setFlameIcon:(UIGestureRecognizer *)gr {
+    NSLog(@"Recognized tap");
+
+    // Base Image
+    GPUImagePicture *inputGPUImage = [[GPUImagePicture alloc] initWithImage:_facebookImage];
+
+    // Flame Image
+    CGPoint point = [gr locationInView:_photoImageView];
+    UIImage *overlayImage = [self createFlame:_photoImageView.frame.size atTouchPoint:point];
+    GPUImagePicture *flameyGPUImage = [[GPUImagePicture alloc] initWithImage:overlayImage];
+
+    // 2. Set up the filter chain
+    GPUImageAlphaBlendFilter * alphaBlendFilter = [[GPUImageAlphaBlendFilter alloc] init];
+    alphaBlendFilter.mix = 1.0;
+
+    [inputGPUImage addTarget:alphaBlendFilter atTextureLocation:0];
+    [flameyGPUImage addTarget:alphaBlendFilter atTextureLocation:1];
+
+    [alphaBlendFilter useNextFrameForImageCapture];
+    [inputGPUImage processImage];
+    [flameyGPUImage processImage];
+
+    UIImage *processedImage = [alphaBlendFilter imageFromCurrentFramebuffer];
+
+    FLPhoto *processedPhoto = [[FLPhoto alloc] init];
+    processedPhoto.image = processedImage;
+    FLProcessedImagesStore *processedImagesStore = [FLProcessedImagesStore sharedStore];
+    [processedImagesStore addUniquePhoto:processedPhoto];
+
+    [_photoImageView setImage:processedImage];
+}
+
+- (UIImage *)createFlame:(CGSize)inputSize atTouchPoint:(CGPoint)touchPoint {
+    UIImage * ghostImage = [UIImage imageNamed:@"ghost.png"];
+
+    CGFloat flameyIconAspectRatio = ghostImage.size.width / ghostImage.size.height;
+
+    NSInteger targetFlameyWidth = inputSize.width * 0.2;
+    CGSize flameSize = CGSizeMake(targetFlameyWidth, targetFlameyWidth / flameyIconAspectRatio);
+
+    // TODO: This is an incorrect way to adjust for touch recognition offset
+    CGPoint newPoint = CGPointMake(touchPoint.x * 0.85, touchPoint.y * 0.85);
+
+    CGRect flameRect = {newPoint, flameSize};
+
+    UIGraphicsBeginImageContext(inputSize);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    CGRect inputRect = {CGPointZero, inputSize};
+    CGContextClearRect(context, inputRect);
+
+    CGAffineTransform flip = CGAffineTransformMakeScale(1.0, -1.0);
+    CGAffineTransform flipThenShift = CGAffineTransformTranslate(flip,0,-inputSize.height);
+    CGContextConcatCTM(context, flipThenShift);
+    CGRect transformedGhostRect = CGRectApplyAffineTransform(flameRect, flipThenShift);
+
+    CGContextDrawImage(context, transformedGhostRect, [ghostImage CGImage]);
+
+    UIImage *flameyIcon = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return flameyIcon;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
