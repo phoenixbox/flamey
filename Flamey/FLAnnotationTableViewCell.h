fa1559c7cae35435edea6ed404eb5dbc33640633
Shane Rogers <rogerssh@tcd.ie>
2015-02-15 14:49:34 -0800

    Custom annotation table view cell

---
+++ Flamey/FLAnnotationTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  FLAnnotationTableViewCell.h
+//  Flamey
+//
+//  Created by Shane Rogers on 2/15/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLAnnotationTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *selectedImageViewBackground;
+
+@end
---
+++ Flamey/FLAnnotationTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  FLAnnotationTableViewCell.m
+//  Flamey
+//
+//  Created by Shane Rogers on 2/15/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import "FLAnnotationTableViewCell.h"
+
+@implementation FLAnnotationTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
