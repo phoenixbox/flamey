1d53661dbed25db20d2ca34c95670c3d779a7b8c
Shane Rogers <rogerssh@tcd.ie>
2015-03-02 23:36:58 -0800

    Tutorial View Controller base

---
+++ Flamey/FLTutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLTutorialViewController.h
+//  Flamey
+//
+//  Created by Shane Rogers on 3/2/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLTutorialViewController : UIViewController
+
+@end
