4ad4aa36d7068aa939a177f663be7937c49d704d
Shane Rogers <rogerssh@tcd.ie>
2015-03-11 19:13:56 -0700

    Add tutorial "How" view

---
+++ Flamey/FLTutorialHowView.m
@@ -0,0 +1,21 @@
+//
+//  FLTutorialHowView.m
+//  Flamey
+//
+//  Created by Shane Rogers on 3/8/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import "FLTutorialHowView.h"
+
+@implementation FLTutorialHowView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
