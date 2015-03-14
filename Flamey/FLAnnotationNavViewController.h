749724e506afc3cf7d9f5d92ec1acd1da46a1d6f
Shane Rogers <rogerssh@tcd.ie>
2015-03-14 12:13:22 -0700

    Subclass the annotation nav view controller for tab bar control

---
+++ Flamey/FLAnnotationNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLAnnotationNavViewController.h
+//  Flamey
+//
+//  Created by Shane Rogers on 3/14/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLAnnotationNavViewController : UINavigationController
+
+@end
---
+++ Flamey/FLAnnotationNavViewController.m
@@ -0,0 +1,37 @@
+//
+//  FLAnnotationNavViewController.m
+//  Flamey
+//
+//  Created by Shane Rogers on 3/14/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import "FLAnnotationNavViewController.h"
+
+@interface FLAnnotationNavViewController ()
+
+@end
+
+@implementation FLAnnotationNavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
