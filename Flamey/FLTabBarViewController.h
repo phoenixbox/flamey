e87acd1e46119a677e3d7ab42e168ed293437066
Shane Rogers <rogerssh@tcd.ie>
2015-03-14 13:18:21 -0700

    Subclass tab bar controller and set custom images

---
+++ Flamey/FLTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLTabBarViewController.h
+//  Flamey
+//
+//  Created by Shane Rogers on 3/14/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLTabBarViewController : UITabBarController
+
+@end
---
+++ Flamey/FLTabBarViewController.m
@@ -0,0 +1,58 @@
+//
+//  FLTabBarViewController.m
+//  Flamey
+//
+//  Created by Shane Rogers on 3/14/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
+//
+
+#import "FLTabBarViewController.h"
+
+@interface FLTabBarViewController ()
+
+@end
+
+@implementation FLTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.tabBar setBackgroundColor:[UIColor redColor]];
+    [self styleTabBar:self.tabBar];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)styleTabBar:(UITabBar *)tabBar {
+    [[UITabBar appearance] setBarTintColor:[UIColor whiteColor]];
+    [[UITabBar appearance] setTintColor:[UIColor blackColor]];
+
+    NSArray *tabBarImagesMap = @[@"Frame", @"Settings"];
+
+    [[tabBar items] enumerateObjectsUsingBlock:^(UITabBarItem *item, NSUInteger index, BOOL *stop){
+        [self setTabItemImages:item forImageName:[tabBarImagesMap objectAtIndex:index]];
+    }];
+}
+
+- (void)setTabItemImages:(UITabBarItem *)item forImageName:(NSString *)imageName {
+    [item setImage:[[UIImage imageNamed:[imageName stringByAppendingString:@"Deselected.png"]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
+    [item setSelectedImage:[[UIImage imageNamed:[imageName stringByAppendingString:@"Selected.png"]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
+    [item setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor grayColor] } forState:UIControlStateNormal];
+    [item setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor blackColor] } forState:UIControlStateHighlighted];
+    [item setTitleTextAttributes:@{ NSForegroundColorAttributeName : [UIColor grayColor], NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Regular" size:12.0] } forState:UIControlStateNormal];
+};
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
