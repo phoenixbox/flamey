3eef016cfb503eec40942416ca1dc7b3bb2df4ed
Shane Rogers <rogerssh@tcd.ie>
2015-03-20 22:32:24 -0700

    Implemented varied copy and title heights

--- Flamey/FLTutorialResultView.m
+++ Flamey/FLTutorialResultView.m
@@ -21,15 +21,23 @@
 }
 
 - (void)setLabels {
+    [self setGetMatchedTitleCopy];
     [self setMatchTitleCopy];
+}
+
+- (void)setGetMatchedTitleCopy {
+    NSDictionary *copySizes = [self copySizes];
+    float matchTitleSize = [[copySizes objectForKey:@"matchTitleSize"] floatValue];
+    _getMatchedTitle.font = [UIFont fontWithName:@"Rochester" size:matchTitleSize];
 }
 
 - (void)setMatchTitleCopy {
     NSString *copy = @"stndout more - get matched more";
     NSDictionary *copySizes = [self copySizes];
-    float fontSize = [[copySizes objectForKey:@"matchTitleSize"] floatValue];
 
-    _matchTitle.font = [UIFont fontWithName:@"AvenirNext-Regular" size:fontSize];
+    float bodyCopySize = [[copySizes objectForKey:@"bodyCopySize"] floatValue];
+
+    _matchTitle.font = [UIFont fontWithName:@"AvenirNext-Regular" size:bodyCopySize];
     _matchTitle.textColor = [UIColor blackColor];
     _matchTitle.lineBreakMode = NSLineBreakByWordWrapping;
     _matchTitle.numberOfLines = 3;
@@ -40,7 +48,7 @@
         NSRange boldRange = [[mutableAttributedString string] rangeOfString:@"stndout" options:NSCaseInsensitiveSearch];
 
         // Core Text APIs use C functions without a direct bridge to UIFont. See Apple's "Core Text Programming Guide" to learn how to configure string attributes.
-        UIFont *boldSystemFont = [UIFont fontWithName:@"AvenirNext-Bold" size:fontSize];
+        UIFont *boldSystemFont = [UIFont fontWithName:@"AvenirNext-Bold" size:bodyCopySize];
         CTFontRef font = CTFontCreateWithName((__bridge CFStringRef)boldSystemFont.fontName, boldSystemFont.pointSize, NULL);
         if (font) {
             [mutableAttributedString addAttribute:(NSString *)kCTFontAttributeName value:(__bridge id)font range:boldRange];
@@ -67,15 +75,13 @@
 
 - (NSDictionary *)copySizes {
     float matchTitleFontSize;
-    float bodycopySize;
-    float buttonCopySize;
 
     if ([UIScreen mainScreen].bounds.size.height == 480) {
         // iPhone 4 - 3.5
-        matchTitleFontSize = 48.0f;
+        matchTitleFontSize = 52.0f;
     } else if ([UIScreen mainScreen].bounds.size.height == 568) {
         // iPhone 5 - 4in
-        matchTitleFontSize = 50.0f;
+        matchTitleFontSize = 57.0f;
     } else if ([UIScreen mainScreen].bounds.size.width == 375) {
         // iPhone 6 - 4.7in
         matchTitleFontSize = 62.0f;
@@ -87,9 +93,13 @@
         matchTitleFontSize = 80.0f;
     }
 
+    float bodyCopySize = [FLViewHelpers bodyCopyForScreenSize];
+    // TODO: Design Button Sizes
+    float buttonCopySize = [FLViewHelpers bodyCopyForScreenSize];
+
     NSDictionary *fontSizes = @{
              @"matchTitleSize": @(matchTitleFontSize),
-             @"bodyCopySize": @(bodycopySize),
+             @"bodyCopySize": @(bodyCopySize),
              @"buttonCopySize": @(buttonCopySize)
              };
 
