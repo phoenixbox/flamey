3bf0d68ab390f9ddfca7520badc37a5b71368df4
Shane Rogers <rogerssh@tcd.ie>
2015-04-11 19:03:47 -0700

    Add FBLApp constants

--- Stndout/FBLAppConstants.h
+++ Stndout/FBLAppConstants.h
@@ -9,5 +9,46 @@
 #ifndef Stndout_FBLAppConstants_h
 #define Stndout_FBLAppConstants_h
 
+#define		PF_STUB_USER_ID                     @"0000000000001"
+#define		PF_STUB_USER_FULLNAME               @"Shane Rogers"
+#define		PF_INSTALLATION_CLASS_NAME			@"_Installation"		//	Class name
+#define		PF_INSTALLATION_OBJECTID			@"objectId"				//	String
+#define		PF_INSTALLATION_USER				@"user"					//	Pointer to User Class
+//-----------------------------------------------------------------------
+#define		PF_USER_CLASS_NAME					@"_User"				//	Class name
+#define		PF_USER_OBJECTID					@"objectId"				//	String
+#define		PF_USER_USERNAME					@"username"				//	String
+#define		PF_USER_PASSWORD					@"password"				//	String
+#define		PF_USER_EMAIL						@"email"				//	String
+#define		PF_USER_EMAILCOPY					@"emailCopy"			//	String
+#define		PF_USER_FULLNAME					@"fullname"				//	String
+#define		PF_USER_FULLNAME_LOWER				@"fullname_lower"		//	String
+#define		PF_USER_FACEBOOKID					@"facebookId"			//	String
+#define		PF_USER_PICTURE						@"picture"				//	File
+#define		PF_USER_THUMBNAIL					@"thumbnail"			//	File
+//-----------------------------------------------------------------------
+#define		PF_CHAT_CLASS_NAME					@"Chat"					//	Class name
+#define		PF_CHAT_USER						@"user"					//	Pointer to User Class
+#define		PF_CHAT_GROUPID						@"groupId"				//	String
+#define		PF_CHAT_TEXT						@"text"					//	String
+#define		PF_CHAT_PICTURE						@"picture"				//	File
+#define		PF_CHAT_VIDEO						@"video"				//	File
+#define		PF_CHAT_CREATEDAT					@"createdAt"			//	Date
+//-----------------------------------------------------------------------
+#define		PF_GROUPS_CLASS_NAME				@"Groups"				//	Class name
+#define		PF_GROUPS_NAME						@"name"					//	String
+//-----------------------------------------------------------------------
+#define		PF_MESSAGES_CLASS_NAME				@"Messages"				//	Class name
+#define		PF_MESSAGES_USER					@"user"					//	Pointer to User Class
+#define		PF_MESSAGES_GROUPID					@"groupId"				//	String
+#define		PF_MESSAGES_DESCRIPTION				@"description"			//	String
+#define		PF_MESSAGES_LASTUSER				@"lastUser"				//	Pointer to User Class
+#define		PF_MESSAGES_LASTMESSAGE				@"lastMessage"			//	String
+#define		PF_MESSAGES_COUNTER					@"counter"				//	Number
+#define		PF_MESSAGES_UPDATEDACTION			@"updatedAction"		//	Date
+
+#define		NOTIFICATION_APP_STARTED			@"NCAppStarted"
+#define		NOTIFICATION_USER_LOGGED_IN			@"NCUserLoggedIn"
+#define		NOTIFICATION_USER_LOGGED_OUT		@"NCUserLoggedOut"
 
 #endif
