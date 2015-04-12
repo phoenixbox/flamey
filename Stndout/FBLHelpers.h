01b0696a2e9355f166957ea101266f27e3ef62e4
Shane Rogers <rogerssh@tcd.ie>
2015-04-12 15:50:49 -0700

    Add login permission requests and helpers

--- Stndout/FBLHelpers.h
+++ Stndout/FBLHelpers.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 void    LoginUser(id target);
 void    PostNotification(NSString *notification);
 NSString *TimeElapsed (NSTimeInterval seconds);
+UIImage* CropImage(UIImage *image, CGFloat x, CGFloat y, CGFloat width, CGFloat height);
+UIImage*		SquareImage					(UIImage *image, CGFloat size);
+UIImage*		ResizeImage					(UIImage *image, CGFloat width, CGFloat height);
+UIImage*		CropImage					(UIImage *image, CGFloat x, CGFloat y, CGFloat width, CGFloat height);
