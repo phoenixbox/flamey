edfc55642161454d711e04e38cd5ecd699709e65
Shane Rogers <rogerssh@tcd.ie>
2015-04-11 19:03:59 -0700

    Create message controller

--- Stndout/FBLMessageController.h
+++ Stndout/FBLMessageController.h
@@ -1,13 +1,18 @@
 //
-//  FBLMessage.h
+//  FBLMessageController.h
 //  Stndout
 //
 //  Created by Shane Rogers on 4/11/15.
 //  Copyright (c) 2015 REPL. All rights reserved.
 //
 
-#ifndef Stndout_FBLMessage_h
-#define Stndout_FBLMessage_h
+#import <Parse/Parse.h>
 
+NSString*	StartPrivateChat			(PFUser *user1, PFUser *user2);
+NSString*	StartMultipleChat			(NSMutableArray *users);
 
-#endif
+void		CreateMessageItem			(PFUser *user, NSString *groupId, NSString *description);
+void		DeleteMessageItem			(PFObject *message);
+
+void		UpdateMessageCounter		(NSString *groupId, NSString *lastMessage);
+void		ClearMessageCounter			(NSString *groupId);
